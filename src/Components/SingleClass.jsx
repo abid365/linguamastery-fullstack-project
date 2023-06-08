e7237402a0d085adf5df08386ad77b270bc19aac
jawad Bin Azam <abidibnazam@gmail.com>
2023-06-08 18:34:36 +0600

    pupular classes completed

--- src/Components/SingleClass.jsx
+++ src/Components/SingleClass.jsx
@@ -1,23 +1,49 @@
 import React from "react";
+import { BsFillArrowUpRightCircleFill, BsPeopleFill } from "react-icons/bs";
+import { MdTipsAndUpdates } from "react-icons/md";
+import { Link } from "react-router-dom";
 
 const SingleClass = (props) => {
   console.log(props.singleclass);
-  const { language, rating, teacher, enrolled_students, image_url, title } =
-    props.singleclass;
+  const {
+    language,
+    rating,
+    teacher,
+    enrolled_students,
+    image_url,
+    title,
+    description,
+  } = props.singleclass;
 
   return (
     <div>
       <div className="w-72 rounded-3xl px-5 py-4 justify-self-center shadow-lg border-2 border-slate-800">
-        <h1 className="text-xl font-bold w-2/3">{title}</h1>
-        <div className="avatar">
-          <div className="w-10 mask mask-squircle">
-            <img src={image_url} />
-          </div>
+        <div className="flex justify-between">
+          <h1 className="text-lg font-bold w-4/5 text-slate-800">{title}</h1>
+          <div className="badge badge-neutral pt-2">{rating}</div>
         </div>
-        <h1 className="font-semibold">{teacher}</h1>
-
-        <small>{rating}</small>
-        <p>Enrolled Students: {enrolled_students}</p>
+        <p className="my-5 text-xs border-l-2 px-2">
+          <MdTipsAndUpdates className="text-lg text-indigo-400 inline-block mb-2 mr-1 animate-pulse"></MdTipsAndUpdates>
+          {description}
+        </p>
+        {/* avatar and name */}
+        <div className="flex items-center gap-3 my-2">
+          <div className="avatar">
+            <div className="w-12 rounded-full ring">
+              <img src={image_url} />
+            </div>
+          </div>
+          <div>
+            <h1 className="font-semibold text-base">{teacher}</h1>
+            <p className="text-xs">
+              <BsPeopleFill className="inline-block"></BsPeopleFill> Enrolled
+              Students: {enrolled_students}
+            </p>
+          </div>
+          <Link className="text-2xl ml-5">
+            <BsFillArrowUpRightCircleFill></BsFillArrowUpRightCircleFill>{" "}
+          </Link>
+        </div>
       </div>
     </div>
   );
