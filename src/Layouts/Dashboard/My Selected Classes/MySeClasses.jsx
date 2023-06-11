a4977c1b824849833ddd95cca27cbcdce4564d08
jawad Bin Azam <abidibnazam@gmail.com>
2023-06-12 00:48:56 +0600

    retrived data from backend

--- src/Layouts/Dashboard/My Selected Classes/MySeClasses.jsx
+++ src/Layouts/Dashboard/My Selected Classes/MySeClasses.jsx
@@ -1,12 +1,20 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../../../Providers/AuthProvider";
 
 const MySeClasses = () => {
+  const { user } = useContext(AuthContext);
+  const [myClass, setMyClass] = useState([]);
+  const url = `http://localhost:5000/myclass?user=${user?.email}`;
+
+  useEffect(() => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => setMyClass(data));
+  }, []);
   return (
     <div className="h-full w-full mt-10">
       <h1 className="text-center">Show Selected Classses here</h1>
-      <p className="text-xs text-center">
-        Add pay and delete button in every selected class
-      </p>
+      <p className="text-2xl text-center">{myClass.length}</p>
     </div>
   );
 };
