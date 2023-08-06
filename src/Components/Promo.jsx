7f61c4eeecb812908aadcf40f4937a1c099ebffb
jawad Bin Azam <abidibnazam@gmail.com>
2023-08-06 20:54:53 +0600

    saving progress

--- src/Components/Promo.jsx
+++ src/Components/Promo.jsx
@@ -6,7 +6,7 @@ const Promo = () => {
       {/* 1st section of promo */}
       <div className="my-28 grid lg:grid-cols-2 grid-cols-1 justify-between items-center ">
         {/* 1st col */}
-        <div>
+        <div className="lg:order-last">
           <h1 className="text-5xl tracking-wider font-bold my-3 text-[#fe8c40] ps-3">
             <span className="hidden lg:block">free.fun.effective</span>
             <span className="block lg:hidden">free fun effective</span>
@@ -49,7 +49,7 @@ const Promo = () => {
       {/* 3rd section of promo */}
       <div className="my-28 grid lg:grid-cols-2 grid-cols-1 justify-between items-center ">
         {/* 1st col */}
-        <div>
+        <div className="lg:order-last">
           <h1 className="text-5xl tracking-wider font-bold my-3 ps-3 text-[#68d9ae]">
             stay motivated
           </h1>
