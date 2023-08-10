a41f55aa1c6c3f5484d3f3155eb25100ee68597e
jawad Bin Azam <abidibnazam@gmail.com>
2023-08-11 01:32:42 +0600

    home page bug fixing

--- src/Layouts/Main/Main.jsx
+++ src/Layouts/Main/Main.jsx
@@ -1,25 +1,33 @@
-import React, { useEffect, useState } from "react";
+// import React, { useEffect, useState } from "react";
 import { Outlet } from "react-router-dom";
 import NavBar from "../../Shared/Navbar/NavBar";
 import Footer from "../../Shared/Footer/Footer";
-
-const [homeLoader, setHomeLoader] = useState(true);
-useEffect(() => {
-  setTimeout(() => {
-    setHomeLoader(false);
-  }, 3000);
-}, []);
+import Loader from "../../Components/Loader";
+import { useEffect, useState } from "react";
 
 const Main = () => {
+  const [homeLoader, setHomeLoader] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setHomeLoader(false);
+    }, 3000);
+  }, []);
+
   return (
     <div className="flex flex-col min-h-[100vh]">
-      <div className="sticky top-0 z-50">
-        <NavBar></NavBar>
-      </div>
-      <Outlet></Outlet>
-      <div className="mt-auto">
-        <Footer></Footer>
-      </div>
+      {homeLoader ? (
+        <Loader />
+      ) : (
+        <>
+          <div className="sticky top-0 z-50">
+            <NavBar></NavBar>
+          </div>
+          <Outlet></Outlet>
+          <div className="mt-auto">
+            <Footer></Footer>
+          </div>
+        </>
+      )}
     </div>
   );
 };
